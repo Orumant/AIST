b72fc5009fb9f42637a4e543947a1001d1d718d9
ALPHA\Khoreshko-EV <Khoreshko.E.V@omega.sbrf.ru>
2018-08-27 17:38:49 +0300

    +- Отдельная таблица результатов для IE

--- src/pages/ResultsPage/index/Results.js
+++ src/pages/ResultsPage/index/Results.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import {detect} from 'detect-browser';
 import Notifications from 'react-notification-system-redux';
 import PageContent from '../../_global/PageContent'
 import TestsTable from "../../../containers/ResultsPage/TestTable";
+import TestTableIE from "../../../containers/ResultsPage/TestTableIE";
 import SearchBar from "../../../containers/ResultsPage/SearchBar";
+import {Alert} from "react-bootstrap";
 import './style.css';
 
 class Results extends React.Component {
@@ -17,6 +20,8 @@ class Results extends React.Component {
 
   render() {
     const {fetchOrders, orders, notifications, isFetching} = this.props;
+    const browser = detect();
+    console.log(browser)
 
     const FilterBar = <SearchBar
       key='results-sidebar'
@@ -25,17 +30,27 @@ class Results extends React.Component {
       />;
 
       const Content = [
-        <TestsTable key='results-table' orders={orders}/>,
+        browser.name == 'ie'?
+          <TestTableIE key='results-table' orders={orders}/>:
+          <TestsTable key='results-table' orders={orders}/>,
         <Notifications key='results-notification' notifications={notifications}/>];
 
-    return (
+      const alertMessage = <Alert bsStyle={"danger"}>
+        <h4>Неподдерживаемый браузер</h4>
+        Часть функционала данного модуля не работает. Для использования всех возможностей АИСТ, пожалуйста,
+        установите Google Chrome(версии 61+)
+      </Alert>
+
+    return [
+      browser.name == 'ie'? alertMessage : null,
       <PageContent
         isFilter={true}
         isLoading={isFetching}
         FilterBar={FilterBar}
         content={Content}
       />
-    )
+      ]
+
   }
 }
 
