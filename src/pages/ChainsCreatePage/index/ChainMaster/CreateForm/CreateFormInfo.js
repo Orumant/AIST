64c597e72cc9b10372890d3ac02fe64a85921503
ALPHA\Khoreshko-EV <Khoreshko.E.V@omega.sbrf.ru>
2018-09-14 19:05:40 +0300

    save

---
+++ src/pages/ChainsCreatePage/index/ChainMaster/CreateForm/CreateFormInfo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import deepPurple from "@material-ui/core/colors/deepPurple";
+import Typography from "@material-ui/core/Typography";
+
+export const CreateFormInfo =
+  <div style={{width: "60%", maxWidth: "800px"}}>
+    <Typography variant="headline" color="secondary">Поля отсутствуют</Typography>
+    <div style={{marginTop: "16px"}}>
+      <Typography> Вы можете добавить поля на форму или продолжить создание цепочки без формы. </Typography>
+      <ul style={{listStyleType: "disc", paddingLeft: "24px"}}>
+        <li className="ul-list-item">
+          <Typography>Для того, чтобы добавить новое поле нажмите на кнопку
+            <span style={{color: "#f50057"}}><b> Добавить поле</b></span>
+          </Typography>
+        </li>
+        <li className="ul-list-item">
+          <Typography>Для того, чтобы продолжить создание цепочки нажмите на кнопку
+            <span style={{color: "#303f9f"}}><b> Вперед</b></span>
+          </Typography>
+        </li>
+      </ul>
+
+    </div>
+  </div>;
