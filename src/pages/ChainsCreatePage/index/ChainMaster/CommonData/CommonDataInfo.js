191ed4b90b109a02625778f985a8f017e6588628
ALPHA\Khoreshko-EV <Khoreshko.E.V@omega.sbrf.ru>
2018-09-24 18:25:48 +0300

    Исправлено инфо мастера

--- src/pages/ChainsCreatePage/index/ChainMaster/CommonData/CommonDataInfo.js
+++ src/pages/ChainsCreatePage/index/ChainMaster/CommonData/CommonDataInfo.js
@@ -2,21 +2,22 @@ import React from 'react';
 import Typography from "@material-ui/core/Typography";
 
 export const CommonDataInfo =
-  <div className="info-form" style={{width: "60%", maxWidth: "400px", height: "100%", maxHeight: "320px"}}>
+  <div className="info-form" style={{width: "60%", maxWidth: "500px", height: "100%", maxHeight: "360px"}}>
     <div style={{ marginTop: "24px", textAlign: 'right'}}>
       <Typography variant="title" style={{color: "rgba(67, 136, 204, 0.7)"}}>Инфо</Typography>
     </div>
     <div style={{marginBottom: "24px"}}>
       <Typography variant="subheading">Название</Typography>
-      <span>Просто название цепочки</span>
+      <span>Имя цепочки используется для запуска цепочки и поиска результатов запуска.</span>
     </div>
     <div className="info-block">
       <Typography variant="subheading">Маркер</Typography>
-      <span>Название сущности, которую генерирует цепочка</span>
+      <span>Маркер определяет тип создаваемых цепочкой тестовых данных (Дебетовая карта, Ипотечный кредит и т.п.)</span>
     </div>
     <div className="info-block">
       <Typography variant="subheading">Группы</Typography>
-      <span>Группы пользователей, которые смогут просматривать и копировать созданную цепочку</span>
+      <span>Группы доступа определяют список пользователей, имеющих право на редактирование цепочки.
+        Можно выбрать несколько групп. При указании группы public - цепочка доступна всем.</span>
       <div style={{color: "#f50057"}}>В списке отображаются только группы, в которые вы входите</div>
     </div>
   </div>;
