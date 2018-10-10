fce6ae999011ded523a4239c5eb195e06f58e8cd
pupyshev-as <pupyshev.a.se@omega.sbrf.ru>
2018-10-10 09:18:57 +0300

    TestsViewer * добавлена кнопка "Создать"

--- src/pages/TestsViever/index/TestsViewer.js
+++ src/pages/TestsViever/index/TestsViewer.js
@@ -5,6 +5,9 @@ import SearchBar from "../../../containers/TestViewer/SearchBar";
 import Notifications from "react-notification-system-redux";
 import FilterByName from "./TestsViewer/FilterByName";
 import ClearIcon from '@material-ui/icons/Clear';
+import Button from "@material-ui/core/Button/Button";
+import {Link} from "react-router-dom";
+import AddIcon from "@material-ui/icons/Add";
 
 const style = {
   width: '100%',
@@ -32,16 +35,26 @@ class TestsViewer extends Component {
   render() {
     const {isLoading, tests, notifications, filterTests} = this.props;
 
-    const tools =
-      <div style={{width: '50%'}} key={'SearchByNameField'}>
+    const tools = [
+      <div style={{width: '100%'}} key={'SearchByNameField'}>
         <FilterByName id={'searchTestsByNameInputField'}
                       onChange={this.handleNameSearching}
-                      value={this.state.name} style={{width: '90%'}}/>
+                      value={this.state.name} style={{width: '50%'}}/>
         {this.state.name.length > 0 &&
         <ClearIcon onClick={() => this.handleNameSearching('')}
                    style={{width: '10px !important', cursor: 'pointer'}}/>}
-      </div>;
-    const filterBar = <SearchBar tests={tests} submit={filterTests} startRequest={{}}/>;
+      </div>,
+      <Button component={Link}
+              key={'create-test-button'}
+              to={'/test/create'}
+              title="Создать">
+        <AddIcon style={{marginRight: '8px'}}/>
+        Создать
+      </Button>
+    ];
+    const filterBar = <SearchBar tests={tests}
+                                 submit={filterTests}
+                                 startRequest={{}}/>;
 
     const content = [
       <TestsTable key={'TestsViewerTable'} tests={tests}/>,
