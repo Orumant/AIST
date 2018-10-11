42cc80040e127b9ce463ad275f0e00073903974f
ALPHA\Khoreshko-EV <Khoreshko.E.V@omega.sbrf.ru>
2018-10-11 11:34:34 +0300

    Стили

---
+++ src/pages/_global/elements/Table/style.js
@@ -0,0 +1,11 @@
+export const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    width: '100%',
+    minWidth: '450px'
+  },
+});
